5094a8a973bef5b4e02bdaf2f4526da2171e17d0
Luckyrana9810 <138463443+Luckyrana9810@users.noreply.github.com>
2025-01-30 03:20:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -55,7 +55,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/luckyrana9810/hutao",
+    "https://github.com/luckyrana9810/hutaodaxx",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
